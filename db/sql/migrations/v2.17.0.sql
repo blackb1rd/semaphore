a876c68d9aa1b0b210243c4c18b231c7c8f95911
Denis Gukov <denguk@gmail.com>
2025-09-23 12:06:01 +0500

    feat(views): add sorting column

--- db/sql/migrations/v2.17.0.sql
+++ db/sql/migrations/v2.17.0.sql
@@ -2,6 +2,8 @@
 alter table project__view add column `hidden` boolean not null default false;
 alter table project__view add column `type` varchar(20) not null default '';
 alter table project__view add column `filter` varchar(1000);
+alter table project__view add column `sort_column` varchar(100);
+alter table project__view add column `sort_reverse` boolean not null default false;
 
 -- Create All view with position -1 for each existing project
 insert into project__view (project_id, title, position, hidden, type)
