73f24bc64e5e124f3249ed510f30b4fc0ced2e7d
Denis Gukov <denguk@gmail.com>
2025-09-27 11:22:27 +0500

    feat(views): add columns

--- db/View.go
+++ db/View.go
@@ -8,12 +8,15 @@ const (
 )
 
 type View struct {
-	ID        int      `db:"id" json:"id" backup:"-"`
-	ProjectID int      `db:"project_id" json:"project_id" backup:"-"`
-	Title     string   `db:"title" json:"title"`
-	Position  int      `db:"position" json:"position"`
-	Type      ViewType `db:"type" json:"type,omitempty"`
-	Hidden    bool     `db:"hidden" json:"hidden,omitempty"`
+	ID          int                `db:"id" json:"id" backup:"-"`
+	ProjectID   int                `db:"project_id" json:"project_id" backup:"-"`
+	Title       string             `db:"title" json:"title"`
+	Position    int                `db:"position" json:"position"`
+	Type        ViewType           `db:"type" json:"type,omitempty"`
+	Hidden      bool               `db:"hidden" json:"hidden,omitempty"`
+	Filter      *MapStringAnyField `db:"filter" json:"filter,omitempty"`
+	SortColumn  *string            `db:"sort_column" json:"sort_column,omitempty"`
+	SortReverse bool               `db:"sort_reverse" json:"sort_reverse,omitempty"`
 }
 
 func (view *View) Validate() error {
